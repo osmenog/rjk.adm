d344efef47792b0c7d5b0325e92c7c001399558b
Dmitry <osmenog@gmail.com>
2014-12-24 18:08:16 +0600

    Разработка модуля синхронизации

--- src/classes/UserSync.php
+++ src/classes/UserSync.php
@@ -22,6 +22,10 @@ function do_sync() {
       print_table($logins_with_names, "Конфликтные пользователи", 3);
     }
   }
+  function users_compare (array $a, array $b) {
+    //Функция необходима для array_udiff
+    return strcmp($a['login'], $b['login'] );
+  }
 
   global $config;
   echo "<h1>SYNC!</h1>";
@@ -31,6 +35,7 @@ function do_sync() {
 
     //Получаем список пользователей SAMS
     $sams_users = $sp->get_sams_logins();
+    //echo "<pre>"; print_r($sams_users); echo "</pre>";
 
     if (count($sams_users) == 0) {
       echo "<p>База данных SAMS пуста. Нечего копировать</p>\n";
@@ -40,41 +45,47 @@ function do_sync() {
     //Получаем пользователей режика, привязанных к серверу.
     //Получаем ВСЕХ пользователей REJIK DB
     $rejik_users = $sp->get_rejik_logins();
+    //echo "<pre>"; print_r($rejik_users); echo "</pre>";
 
     //Формируем список пользователей, которых нужно перенести слева -> направо
     // (пользователи, которые были созданы в SAMS)
     //$users_to_copy   = array_diff ($sams_users, $rejik_users);
-    foreach ($sams_users as $k => $v) {
-      //fixme https://php.net/manual/ru/function.array-udiff.php
-    }
-    // (пользователи, которые были удалены в SAMS, но остиались в REJIKDB
-    $users_to_remove = array_diff ($rejik_users, $sams_users);
+    $users_to_copy = array_udiff ($sams_users, $rejik_users, "users_compare");
 
+    // (пользователи, которые были удалены в SAMS, но остиались в REJIKDB
+    $users_to_remove = array_udiff ($rejik_users, $sams_users, "users_compare");
 
     //Проверяем, были ли пользователи $users_to_copy добавлены в REJIK DB ранее, но для других прокси.
     $conflict_users = $sp->check_users_for_other_pids($users_to_copy);
 
     //Удаляем $conflict_users из списка пользователей на копирование
-    foreach ($users_to_copy as $n => $u_login) {
-      foreach ($conflict_users as $c_login => $pid) {
-        if ($u_login == $c_login) unset ($users_to_copy[$n]);
+    if (count($conflict_users) != 0) {
+      foreach ($users_to_copy as $k => $row) {
+        foreach ($conflict_users as $c_row) {
+          if ($row['login'] == $c_row['login']) unset ($users_to_copy[$k]);
+        }
       }
     }
 
-    echo "<pre>"; print_r($users_to_copy); echo "</pre>";
-    echo "<pre>"; print_r($users_to_remove); echo "</pre>";
+    //echo "<pre>"; print_r($users_to_copy); echo "</pre>";
+    //echo "<pre>"; print_r($users_to_remove); echo "</pre>";
     echo "<pre>"; print_r($conflict_users); echo "</pre>";
 
     //Выводим на экран различную информацию о ходе синхронизации.
     //display_content($sp, $users_to_copy, $users_to_remove, $conflict_users);
 
     //Копируем подготовленных пользователей в REJIK DB
-    //$sp->copy_to_rejik($users_to_copy);
+    $sp->copy_to_rejik($users_to_copy);
+
+    //Подключаем конфликтных пользователей к нашему прокси
+    $sp->connect_users_to_proxy($conflict_users);
 
   } catch (Exception $e) {
       echo "<div class='alert alert-danger'><b>Ошибка</b> {$e->getCode()} : {$e->getMessage()}<br/><pre>{$e->getTraceAsString()}</pre></div>\n";
   }
 
+
+  exit;
   return True;
 }
 function print_table($printable_array, $title, $id) {
@@ -91,10 +102,10 @@ function print_table($printable_array, $title, $id) {
   echo "<div id='accordion{$id}' class='panel-collapse collapse'>";
   echo "<table id='table-sync' class='table table-condensed small'>\n";
 
-  foreach ($printable_array as $k => $v) {
+  foreach ($printable_array as $row) {
     echo "<tr>";
-    echo "<td>{$k}</td>";
-    echo "<td>{$v}</td>";
+    echo "<td>{$row['login']}</td>";
+    echo "<td>{$row['name']}</td>";
     echo "</tr>\n";
   }
   echo "</table>\n";
@@ -136,6 +147,11 @@ class sams_sync {
     $this->server_id = $server_id;
   }
 
+  public function __destruct() {
+    $this->sams_conn->closedb();
+    $this->rejik_conn->closedb();
+  }
+
   /**
    * Функция получает список логинов пользователей SAMS
    * @return array Возвращает список логинов в виде миссива:
@@ -173,7 +189,7 @@ class sams_sync {
     foreach ($rejik_data as $row) {
       //Если пользовательь относится к текущему серверу, добавляем его в массив. Остальных игнорируем
       if ($row['proxy_id'] == $this->server_id) {
-        $res[]=array ("login" => $row['login']);
+        $res[]=array ("id" => $row['id'], "login" => $row['login']);
       }
     }
 
@@ -229,9 +245,13 @@ class sams_sync {
       //Получаем полную инфу о пользователях SAMS
       $sams_data = $this->get_sams_userdata();
 
-      foreach ($users as $v) {
+      foreach ($users as $row) {
+        $v = $row['login'];
         if (array_key_exists($v, $sams_data)) {
-          $res[$v] = $sams_data[$v]['family'] . " " . $sams_data[$v]['name'] . " " . $sams_data[$v]['soname'];
+          $res[] = array (
+                    "login" => $v,
+                    "name" => trim($sams_data[$v]['family'] . " " . $sams_data[$v]['name'] . " " . $sams_data[$v]['soname'])
+                   );
         }
       }
     } elseif ($source == SOURCE_RDB) {
@@ -239,74 +259,21 @@ class sams_sync {
       $rejik_data = $this->get_rejik_userdata();
       //fixme asdasdasdasd
 
-      foreach ($users as $k => $v) {
-        foreach ($rejik_data as $u) {
+      foreach ($users as $row) {
+        $v = $row['login'];
+        foreach ($rejik_data as $k => $u) {
           if ($v == $u['login']) {
-            $res[$v] = isset($rejik_data[$k]['name']) ? $rejik_data[$k]['name'] : "-";
+            $res[] = array (
+                      "login" => $v,
+                      "name"  => isset($rejik_data[$k]['name']) ? $rejik_data[$k]['name'] : "-"
+                     );
           }
         }
       }
     }
-    print_r ($res);
+
     return $res;
   }
-  /**
-   * Получает информацию о пользователях SAMS, сохраняя ее для дальнейшего использования.
-   * @return array Возвращает массив в виде:
-   * array ( [0] => "aaa", [1] => "bbb", ... ,  [n] => "ccc"  )
-   * @throws Exception Выбрасывает в случае, если API функция завершилась с ошибкой
-   */
-/*  public function _get_sams_logins(){
-    try {
-      //С помощью класса PROXY WORKER получаем список пользователей самс в ПОЛНОМ виде
-      $sams_full_users = $this->sams_conn->get_userslist(FIELDS_FULL);
-
-      //Если в БД SAMS нет ни одного пользователя, то возвращаем 0
-      if ($sams_full_users == 0) return 0;
-      //Если произошла другая ошибка, то вызываем исключение
-      if (!$sams_full_users) throw new Exception("Произошла ошибка при получении пользователей SAMS");
-
-      $this->sams_users_full = $sams_full_users;
-
-      //Заполняем результырующий массив логинами пользователей SAMS
-      $res = array();
-      foreach ($sams_full_users as $k => $r) {
-        $res[$k] = $r['nick'];
-      }
-    } catch (Exception $e) {
-      throw $e;
-    }
-
-    return $res;
-  }*/
-
-  /**
-   * Функция получает список ВСЕХ пользователей REJIK DB, сохраняя их для дальнейшего использования.
-   * @return array Возвращает массив с логинами пользователей в виде:
-   * array ( [0] => "aaa", [1] => "bbb", ... ,  [n] => "ccc"  )
-   * @throws Exception
-   * @throws mysql_exception
-   */
-/*  public function _get_from_rejik() {
-    //Получаем ВСЕХ пользователей REJIK DB
-    $rejik_full_users = $this->rejik_conn->users_get(FIELDS_FULL);
-
-    if ($rejik_full_users == 0) return 0;
-    if (!$rejik_full_users) throw new Exception("Произошла ошибка при получении пользователей REJIK DB");
-
-    $this->rejik_users_full = $rejik_full_users;
-
-    $res = array();
-    //Перебираем список пользователей
-    foreach ($rejik_full_users as $row) {
-      //Если пользовательь относится к текущему серверу, добавляем его в массив. Остальных игнорируем
-      if ($row['proxy_id'] == $this->server_id) {
-        $res[]=$row['login'];
-      }
-    }
-
-    return $res;
-  }*/
 
   /**
    * Функция сравнивает списки пользователей, и возвращает пользователей,
@@ -321,7 +288,7 @@ class sams_sync {
 
     if (count($users_to_copy) == 0) return array();
 
-    //Берем данные из кеша, или получаем данные о пользователях заного.
+    //Берем данные из кеша, или получаем данные о пользователях заново.
     if (!$this->rejik_data_cahced) {
       $rejik_data = $this->get_rejik_userdata();
     } else {
@@ -335,7 +302,7 @@ class sams_sync {
     foreach ($users_to_copy as $v) {
       // ... и проверяем, был ли пользователь добавлен на текущий сервер ранее.
       foreach ($rejik_data as $row) {
-        if (($row['login'] == $v) && ($row['proxy_id'] != $this->server_id))  $res[] = array("login" => $v, "proxy_id" => $row['proxy_id']);
+        if (($row['login'] == $v['login']) && ($row['proxy_id'] != $this->server_id))  $res[] = array("id" => $row['id'], "login" => $v['login'], "proxy_id" => $row['proxy_id']);
       }
     }
 
@@ -343,17 +310,32 @@ class sams_sync {
   }
 
   /**
-   *
-   * @param $users_and_pids
+   * Функция "подключает" пользователей к текущему прокси
+   * @param $users Массив с логинами пользователей и их pid
    */
-  public function connect_user($users_and_pids) {
+  public function connect_users_to_proxy($users) {
+    //Выходим, если массив пустой
+    if (count($users)==0) return FALSE;
 
+    //Обрабвтываем каждого конфликтного пользователя
+    foreach ($users as $row) {
+      //1.Проверяем, был ли пользователь подключен к прокси ранее.
+      //$query = "SELECT `id`, `user_id`, `assign_pid` FROM `users_location` WHERE user_id=1;";
+      //$res = $this->rejik_conn->do_query($query);
+
+
+      //2.Если нет, то подключаем пользователя:
+    }
+
+    return true;
   }
 
   public function copy_to_rejik($users) {
-    //Функция копирует
+
     if (count($users)==0) return FALSE;
-    foreach ($users as $v) {
+
+    foreach ($users as $row) {
+      $v = $row['login'];
       $user_data = $this->sams_userdata[$v];
       $pid = $this->server_id;
       $name = trim($user_data['family']." ".$user_data['name']." ".$user_data['soname']);
